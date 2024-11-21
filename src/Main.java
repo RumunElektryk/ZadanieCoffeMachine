99ddfbae8b61b2c1bc9d3c098b7ac02a85504acc
RumunElektryk <szymonkwoka69@gmail.com>
2024-11-21 14:43:55 +0100

    Ulalala

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+interface CoffeMachine{
+    String prepareCoffee();
+}
+abstract class BaseCoffe implements CoffeMachine{
+    protected int water;
+    protected int coffee;
+    protected int milk;
+}
+public BaseCoffe(int water, int coffee, int milk) {
+    this.water = water;
+    this.coffee = coffee;
+    this.milk = milk;
+
+public Espresso extends BaseCoffe {
+    public Espresso
+
+    }
+}
